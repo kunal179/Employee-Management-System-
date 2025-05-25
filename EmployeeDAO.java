55c5755169e59dbc22d42f8060bfe6e0c3e7d399
kunal bhati <kunal.24scse1011265@galgotiasuniversity.ac.in>
2025-05-25 16:44:21 +0530

    Delete EmployeeDAO.java

--- EmployeeDAO.java
+++
@@ -1,7 +0,0 @@
-public interface EmployeeDAO {
-    void addEmployee(Employee employee);
-    Employee getEmployeeById(int id);
-    List<Employee> getAllEmployees();
-    void updateEmployee(Employee employee);
-    void deleteEmployee(int id);
-}
